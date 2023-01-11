0aabc87947f3dc3db106b0be3fa00a0122e8d14b
Joshua Jones <a21116@alunos.ipca.pt>
2023-01-11 21:07:41 +0000

    I guess

--- Aula 1/Exercicio 2/Exercicio 2.ino
+++ Aula 1/Exercicio 2/Exercicio 2.ino
@@ -1,80 +1,25 @@
-#include <SPI.h>
-#include <Ethernet.h>
+#include <LiquidCrystal.h>
 
-int fan = 4;
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2)
 
-int pos = 0;
-byte mac[] = { 0x90, 0xA2, 0xDA, 0x00, 0x24, 0x70 };
-byte ip[] = { 192,168,1,200 };
-byte gateway[] = { 192, 168, 1, 199 };
-byte subnet[] = { 255, 255, 255, 0 };
-EthernetServer server(80);
+float tempC;
 
-String readString;
+int tempPin = A1;
 
-void setup() {
-    Serial.begin(9600);
-    while (!Serial)
-    {
-      ;
-    }
-
-    pinMode(fan, OUTPUT);
-
-    Ethernet.begin(mac, ip);
-    server.begin();
-    Serial.print("server is at ");
-    Serial.println(Ethernet.localIP())
+void setup(){
+  lcd.begin(16, 2);
+  lcd.print("Temp: ");
+  lcd.setCursor(0, 1);
+  Serial.begin(9600);
 }
 
-void loop() {
-  EthernetClient client = server.available();
+void loop(){
+  tempC = analogRead(tempPin);
+  tempC = (tempC * 5 * 100.0)/1024.0;
 
-  if(client) {
-    while (client.connected()) {
-      if (client.available()) {
-        char c = client.read();
-
-        if (readString.length() < 100) {
-          readString += c;
-        }
-        if ( c == '\n') {
-          Serial.println(readString);
-          client.println("HTTP/1.1 200 OK");
-          client.println("Content-Type: text/html");
-          client.println();
-          client.println("<HTML>");
-          client.println("<HEAD>");
-          client.println("<meta name='apple-mobile-web-app-capable' content='yes' />");
-          client.println("<meta name='apple-mobile-web-app-status-bar-style' content='black-translucent' />");
-          client.println("<Title>Arduino Fan control switch </TITLE>");
-          client.println("</HEAD>");
-          client.println("<BODY>");
-          client.println("<H1>FAN Control</H1>");
-          client.println("<hr />");
-          client.println("<br />");
-          client.println("<H2> Fan control with arduino Ethernet shield</H2>");
-          client.println("<br />");
-          client.println("<a href=\"/?button1on\"\">Turon On FAN</a>");
-          client.println("<a href=\"/?button1off\"\">Turon Off FAN</a><br />");
-          client.println("<br />");
-          client.println("<br />");
-          client.println("<br />");
-          client.println("</BODY>");
-          client.println("</HTML>");
-
-          delay(1);
-          client.stop();
-
-          if (readString.indexOf("?button1off") >0) {
-            digitalWrite(fan, HIGH);
-          }
-          if (readString.indexOf("?button1off" >0)) {
-            digitalWrite(fan, LOW);
-          }
-          readString="";
-        }
-      }
-    }
-  }
+  //print result to lcd display
+  lcd.setCursor(6, 0);
+  lcd.print(tempC, 1);
+  Serial.print("Temperautra: ");
+  
 }
